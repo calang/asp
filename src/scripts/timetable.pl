9f0815fea68efd66ad6e43b49b65afac0a1f7144
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-06-18 23:21:40 -0600

    clean timetable generation

--- src/scripts/timetable.pl
+++ src/scripts/timetable.pl
@@ -30,16 +30,10 @@ timetable(Nivel) :-
                     findall(
                         Materia,
                         (   member(Dia, Dias),
-                            writeln(Nivel),
-                            writeln(Dia),
-                            writeln(Bloquee),
-                            writeln(Leccion),
-                            asignado(Nivel,Materia,_Profe,Dia,Bloquee,Leccion),
-                            writeln(asignado(Nivel,Materia,_Profe,Dia,Bloquee,Leccion))
+                            asignado(Nivel,Materia,_Profe,Dia,Bloquee,Leccion)
                         ),
                         Materias
                     ),
-                    writeln(Materias),
                     format( '~w ~w~t~10|~t~w~t~20|~t~w~t~30|~t~w~t~40|~t~w~t~50|~t~w~t~60|~n',
                             [Bloquee, Leccion | Materias ]
                     )
